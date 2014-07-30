8324af9b26ac25e20de30c37c13fa800a4aba477
Jesus Enrique Peña Sigua <JEnrique.PS@gmail.com>
2014-07-30 17:02:40 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,26 +1,3 @@
-/*************************************************************************
- app.js
- SnakeFox
- V. 1.2
- 	Hiram E. Pérez
- 	@driverInside
-
- 	Copyright (C) 2013  Hiram E. Pérez
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>
-  ************************************************************************/
-
 $(function(){
 
 	$('small').text("Snake v1.3 | @driverInside"); // :P
